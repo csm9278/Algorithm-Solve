2d59ea30b79faa693864768586cb2cf946111f4f
csm9278 <105195278+csm9278@users.noreply.github.com>
2024-05-21 13:43:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.22 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,46 +1,44 @@
 #include<vector>
 #include<queue>
-#include<iostream>
 using namespace std;
 
-bool visited[101][101] = {false};
-int dx[4] = {-1, 1 , 0, 0};
-int dy[4] = {0, 0, -1, 1};
+bool visited[101][101];
+int dy[] = {-1, 1, 0, 0};
+int dx[] = {0, 0, -1, 1};
 
 int solution(vector<vector<int> > maps)
 {
-    int answer = 1;
-    
+    int answer = 0;
     queue<pair<int,int>> ques;
     ques.push({0, 0});
     visited[0][0] = true;
     
-    int xSize = maps[0].size() - 1;
     int ySize = maps.size() - 1;
-    
+    int xSize = maps[0].size() - 1;
     
     while(!ques.empty())
     {
-        int x = ques.front().second;
         int y = ques.front().first;
+        int x = ques.front().second;
         ques.pop();
         
-        for(int i = 0; i < 4; i++)
+        for(int i = 0 ; i < 4; i++)
         {
-            int gx = x + dx[i];
             int gy = y + dy[i];
+            int gx = x + dx[i];
+            
             if(0 <= gy && gy <= ySize && 0 <= gx && gx <= xSize)
+            if(!visited[gy][gx] && maps[gy][gx] > 0)
             {
-                if(visited[gy][gx])
-                    continue;
-                if(maps[gy][gx] <= 0)
-                    continue;
-                ques.push({gy, gx});
-                maps[gy][gx] = maps[y][x] + 1;
                 visited[gy][gx] = true;
+                maps[gy][gx] = maps[y][x] + 1;
+                ques.push({gy, gx});
             }
         }
     }
     
-    return maps[ySize][xSize] == 1 ? -1 : maps[ySize][xSize];
+    if(maps[ySize][xSize] == 1)
+        return -1;
+    else
+        return maps[ySize][xSize];
 }
