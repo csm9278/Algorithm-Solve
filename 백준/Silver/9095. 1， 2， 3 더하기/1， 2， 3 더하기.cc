8f2278b0622a5fb706f3e2e3987b9ef2cc19487e
csm9278 <105195278+csm9278@users.noreply.github.com>
2024-10-24 17:51:48 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int dp[11];
+
+int main()
+{   
+    int n;
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for(int i = 4; i < 11; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]);
+
+    for(int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        cout << dp[num] << endl;
+    }
+    return 0;
+}
