b61b196532f1eb2d0594f3b2e2560dfbfb3d15de
csm9278 <105195278+csm9278@users.noreply.github.com>
2024-10-25 21:01:46 +0900

    [Silver IV] Title: 로프, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-int main() {
+bool cmp(int a, int b)
+{
+    return a > b;
+}
+
+int main()
+{
     int n;
     cin >> n;
-    
     vector<int> vecs(n);
-    for (int i = 0; i < n; i++) {
+
+    for(int i = 0; i < n; i++)
         cin >> vecs[i];
+
+    sort(vecs.begin(), vecs.end(), cmp);
+    int answer = vecs[0];
+    for(int i = 1; i < n; i++)
+    {
+        if(answer < (vecs[i] * (i + 1)))
+            answer = vecs[i] * (i + 1);
     }
-    
-    // 내림차순 정렬
-    sort(vecs.begin(), vecs.end(), greater<int>());
-    
-    int answer = 0;
-    for (int i = 0; i < n; i++) {
-        // 현재까지의 최대 중량 갱신
-        answer = max(answer, vecs[i] * (i + 1));
-    }
-    
-    cout << answer << endl;
-    return 0;
+    cout << answer;
 }
