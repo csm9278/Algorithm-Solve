77de2679dbb4d2083baceda0c8d565196874ad87
csm9278 <105195278+csm9278@users.noreply.github.com>
2025-02-25 15:02:18 +0900

    [Silver V] Title: 카약과 강풍, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2891. 카약과 강풍/카약과 강풍.cc
+++ 백준/Silver/2891. 카약과 강풍/카약과 강풍.cc
@@ -1,65 +1,67 @@
 #include <iostream>
+#include <stdio.h>
 #include <vector>
 using namespace std;
 
 int n, m, k;
-bool bKayak[11]; // 부서진 카약 여부
-bool spare[11];  // 여분 카약 여부
+bool bKayak[11];
+bool sKayak[11];
 
-void Input() {
+void Input()
+{
     cin >> n >> m >> k;
 }
 
-void Solve() {
-    // 부서진 카약 정보 입력
-    for (int i = 0; i < m; i++) {
+void Solve()
+{
+    for(int i = 0; i < m; i++)
+    {
         int broken;
         cin >> broken;
         bKayak[broken] = true;
     }
 
-    // 여분 카약 정보 입력
-    for (int i = 0; i < k; i++) {
+    for(int i = 0; i < k; i++)
+    {
         int spair;
         cin >> spair;
-        spare[spair] = true;
+        sKayak[spair] = true;
     }
 
     // 1. 자기 여분 카약 사용
     for (int i = 1; i <= n; i++) {
-        if (bKayak[i] && spare[i]) {
+        if (bKayak[i] && sKayak[i]) {
             bKayak[i] = false;
-            spare[i] = false;
+            sKayak[i] = false;
         }
     }
+
 
     // 2. 앞 팀 → 뒷 팀 순서로 대여
     for (int i = 1; i <= n; i++) {
         if (bKayak[i]) {
-            if (i - 1 >= 1 && spare[i - 1]) { // 앞 팀 확인
+            if (i - 1 >= 1 && sKayak[i - 1]) { // 앞 팀 확인
                 bKayak[i] = false;
-                spare[i - 1] = false;
+                sKayak[i - 1] = false;
             }
-            else if (i + 1 <= n && spare[i + 1]) { // 뒷 팀 확인
+            else if (i + 1 <= n && sKayak[i + 1]) { // 뒷 팀 확인
                 bKayak[i] = false;
-                spare[i + 1] = false;
+                sKayak[i + 1] = false;
             }
         }
     }
-
-    // 3. 출발 못하는 팀 계산
     int answer = 0;
-    for (int i = 1; i <= n; i++) {
-        if (bKayak[i]) {
+    for(int i = 1; i <= n; i++)
+        if(bKayak[i])
             answer++;
-        }
-    }
 
-    cout << answer << "\n";
+    cout << answer;
 }
 
-int main() {
+int main()
+{
     Input();
     Solve();
+
     return 0;
 }
